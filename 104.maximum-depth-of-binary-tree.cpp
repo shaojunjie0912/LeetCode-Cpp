8a18be8235ec2fadaf7d2374fba20208e4923355
shaojunjie0912 <shaojunjie0912@outlook.com>
2024-05-13 17:12:10 +0800

    写了平衡二叉树

--- 104.maximum-depth-of-binary-tree.cpp
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -17,18 +17,64 @@ using namespace std;
  * };
  */
 class Solution {
+    // 注意二叉树的 高度 不同于 深度！！！！
+    // 深度：当前结点 到 根结点 的距离（前序遍历）
+    // 高度：当前结点 到 最远叶结点 的距离（后序遍历）
+
+    // 本题之所以前后序都可以是因为二叉树的最大深度就是根节点的高度
+    // 本题求深度用前序反而不易理解
 public:
 #if 1
-    // 递归实现
+    // ---------------------  递归法 --------------------
+    // 后序递归：用求高度求深度
     int maxDepth(TreeNode* root) {
         if (root == nullptr)  // 递归退出条件
             return 0;
+        // 左
         int l_depth = maxDepth(root->left);  // 单层递归逻辑
+        // 右
         int r_depth = maxDepth(root->right);
+        // 根 depth = 1 + max(l_depth, r_depth)
         return max(l_depth, r_depth) + 1;  // 返回最大的+1
     }
+#elif 0
+    // 极简递归后序写法
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+#elif 0
+    // 前序递归：正统求深度，但是很复杂
+    int result;
+    void getdepth(TreeNode* node, int depth) {
+        result = depth > result ? depth : result;  // 中
+
+        if (node->left == NULL && node->right == NULL)
+            return;
+
+        if (node->left) {  // 左
+            depth++;       // 深度+1
+            getdepth(node->left, depth);
+            depth--;  // 回溯，深度-1
+        }
+        if (node->right) {  // 右
+            depth++;        // 深度+1
+            getdepth(node->right, depth);
+            depth--;  // 回溯，深度-1
+        }
+        return;
+    }
+    int maxDepth(TreeNode* root) {
+        result = 0;
+        if (root == NULL)
+            return result;
+        getdepth(root, 1);
+        return result;
+    }
+
 #else
-    // 迭代实现
+    // ---------------------  迭代法 --------------------
     // 使用二叉树的层序遍历
     int maxDepth(TreeNode* root) {
         if (root == nullptr)  // 经典判空
--- 110.balanced-binary-tree.cpp
+++ 110.balanced-binary-tree.cpp
@@ -20,28 +20,32 @@ class Solution {
     // 一个二叉树每个节点的左右两个子树的高度差的绝对值不超过 1
     // 注意二叉树的 高度 不同于 深度！！！！
     // 深度：当前结点 到 根结点 的距离（前序遍历）
-    // 高度：当前结点 到 叶结点 的距离（后序遍历）
+    // 高度：当前结点 到 最远叶结点 的距离（后序遍历）
 public:
-    bool isBalanced(TreeNode* root) {
-        if (root == nullptr)
-            return true;
-        int left_depth = 0;
-        int right_depth = 0;
-        TreeNode* left = root->left;
-        TreeNode* right = root->right;
-        while (left != nullptr) {
-            left = left->left;
-            ++left_depth;
-        }
-        while (right != nullptr) {
-            right = right->right;
-            ++right_depth;
-        }
-        if (std::abs(left_depth - right_depth) <= 1)
-            return true;
+    // 定义一个根据高度判断平衡二叉树的递归函数
+    // 后序递归计算高度
+    int getHeight(TreeNode* root) {
+        if (root == nullptr)  // 空结点高度为 0
+            return 0;
+        int left_height = getHeight(root->left);  // 左-----------
+        if (left_height == -1)                    // 一定要判断，否则都是 -1 无法判错
+            return -1;                            // 左子树不平衡则提前退出
+        int right_height = getHeight(root->right);  // 右---------
+        if (right_height == -1)  // 一定要判断，否则都是 -1 无法判错
+            return -1;           // 右子树不平衡则提前退出
+
+        // 标记是否是二叉平衡树(-1 or height)
+        // 若不是则返回 -1，不需要返回高度
+        // 若是，则返回以当前结点为根节点的二叉树高度
+        // 高度是 1 + 左右子树高度最大的
+        if (abs(left_height - right_height) > 1)  // 根----------
+            return -1;
         else
-            return false;
-        return isBalanced(root->left) && isBalanced(root->right);
+            return 1 + max(left_height, right_height);
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return getHeight(root) == -1 ? false : true;
     }
 };
 // @leet end
